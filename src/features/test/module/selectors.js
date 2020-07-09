9ba515477ced2093efabb97e40cf75394b0ce03c
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-09 17:40:39 +0500

    feat(test): replace plain currentQuestion selector by reselect selector

--- src/features/test/module/selectors.js
+++ src/features/test/module/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect'
+
 export const tickets = (state) => state.test.tickets
 export const isFetching = (state) => state.test.isFetching
 export const error = (state) => state.test.error
@@ -5,8 +7,11 @@ export const error = (state) => state.test.error
 export const currentTicket = (state) => state.test.currentTicket
 export const wrongAnswersCounter = (state) => state.test.wrongAnswersCounter
 export const finishStatus = (state) => state.test.finishStatus
-export const currentQuestionPosition = (state) => state.test.currentQuestionPosition
-export const currentQuestion = (state) =>
-  state.test.currentTicket.find(
-    (question) => question.position === state.test.currentQuestionPosition
-  )
+export const currentQuestionPosition = (state) =>
+  state.test.currentQuestionPosition
+
+export const currentQuestion = createSelector(
+  [currentTicket, currentQuestionPosition],
+  (ticket, position) =>
+    ticket.find((question) => question.position === position)
+)
