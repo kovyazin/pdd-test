55c543bb03a095b7963997c914f41420408f1a32
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-09 18:36:43 +0500

    feat: make not-found page

--- src/pages/not-found/index.js
+++ src/pages/not-found/index.js
@@ -1,10 +1,20 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 export const NotFoundPage = () => {
   return (
-    <Fragment>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </Fragment>
+    <Container>
+      <div className="vh-100 d-flex flex-column align-items-center justify-content-center">
+        <h1>404</h1>
+        <p>Страница не найдена</p>
+        <Link to="/">
+          <Button variant="outline-primary">Вернуться на главную</Button>
+        </Link>
+      </div>
+    </Container>
   )
 }
